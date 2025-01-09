12b5b4afa6c5e7a911df1acac870e2f993193b77
Akshara <152853335+Akshara2453@users.noreply.github.com>
2025-01-09 10:25:56 +0530

    Update background.js

--- EXTENSION/background.js
+++ EXTENSION/background.js
@@ -1,52 +1,5 @@
 
 
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.url) {
-//         console.log("Received image URL for OCR:", request.url);  // Debugging: log the URL received
-//         sendImageToOCR(request.url);  // Send the image directly to the OCR API
-//     }
-// });
-
-// async function sendImageToOCR(imageUrl, originalFileName) {
-//     console.log("Sending image to OCR model:", originalFileName); // Debugging: log the filename sent to OCR
-
-//     const formData = new FormData();
-
-//     try {
-//         const response = await fetch(imageUrl);
-//         if (!response.ok) {
-//             console.error("Failed to fetch image from URL:", imageUrl);
-//             return; // Exit if the fetch fails
-//         }
-
-//         const blob = await response.blob();
-//         formData.append('file', blob, originalFileName);
-
-//         const ocrResponse = await fetch('http://127.0.0.1:5001/predict', {
-//             method: 'POST',
-//             body: formData
-//         });
-
-//         if (!ocrResponse.ok) {
-//             console.error("Failed to send image to OCR:", ocrResponse.statusText);
-//             return; // Exit if the OCR request fails
-//         }
-
-//         const data = await ocrResponse.json();
-//         if (data.predicted_text) {
-//             console.log("OCR Result:", data.predicted_text);
-//             // Send the OCR result to the content script
-//             chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//                 chrome.tabs.sendMessage(tabs[0].id, { type: 'OCR_RESULT', text: data.predicted_text });
-//             });
-//         } else {
-//             console.error("No text found in OCR result");
-//         }
-//     } catch (error) {
-//         console.error("Error sending image to OCR:", error);
-//     }
-// }
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.url) {
         console.log("Received image URL for OCR:", request.url);
